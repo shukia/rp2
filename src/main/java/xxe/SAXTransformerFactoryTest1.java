1b847bdb6350efc8f2fe42e8854a606024682c30
shukia <shuki.avraham@gmail.com>
2021-01-17 12:33:45 +0200

    Add files via upload

--- src/main/java/xxe/SAXTransformerFactoryTest1.java
+++ src/main/java/xxe/SAXTransformerFactoryTest1.java
@@ -1,11 +1,7 @@
 package xxe;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.xml.XMLConstants;
 import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerConfigurationException;
-import javax.xml.transform.TransformerException;
-import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.sax.SAXTransformerFactory;
 import javax.xml.transform.stream.StreamResult;
 import javax.xml.transform.stream.StreamSource;
@@ -33,9 +29,7 @@ public class SAXTransformerFactoryTest1 {
             StreamSource source = new StreamSource(new StringReader(xmlString));
             StringWriter writer = new StringWriter();
             StreamResult target = new StreamResult(writer);
-
             transformer.transform(source, target);
-
             return writer.toString();
         } catch (Exception e) {
             e.printStackTrace();
